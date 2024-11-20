51b609562b26ac71cb6038726437efb8a0329031
open-filer <kritagyasingh2362007@gmail.com>
2024-11-20 21:24:28 +0530

    basic_fun.py

--- basic_fun.py
+++ basic_fun.py
@@ -1,23 +1,18 @@
-# Function for chess board creation
+# to display the chess board
 import chess
-board  = chess.Board()
+board = chess.Board()
 print(board)
 print("\n")
 
-# Making a move1 (white)
-board.push_san("e2e4")
-print(board)
-print("\n")
-# Checking legal move for black
-legal_moves = board.legal_moves
-print(legal_moves)
-print("\n")
+#function to take moves from user 
 
-# Making a move1 (black)
-board.push_san("e7e5")
-print(board)
-print("\n")
-# Checking legal move for white
-legal_moves = board.legal_moves
-print(legal_moves)
-print("\n")
+def make_move():
+    legal_moves = board.legal_moves
+    print(legal_moves,"\n")
+    move = input("Enter your move : ")
+    board.push_san(move)
+    print(board)
+    print("\n")
+    make_move()
+
+make_move()
